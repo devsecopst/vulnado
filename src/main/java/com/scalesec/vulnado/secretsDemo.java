e01ce9b8d12a6443166a77b2f8a776bc3bb0d5a3
devsecopst <devsecopst@gmail.com>
2025-08-08 14:18:08 +0300

    test

--- src/main/java/com/scalesec/vulnado/secretsDemo.java
+++ src/main/java/com/scalesec/vulnado/secretsDemo.java
@@ -10,15 +10,6 @@ public class secretsDemo {
         String githubToken = "ghp_D8fR2sY9ABQpGgW2S3xF4dVYZkNpWqz57TbR";
         String githubToken44 = "ghp_D8fR2sY9ABQpGgW2S3xF4dVYZkNpWqz57TbR";
         
-        
-
-        
-
-
-        
-
-
-
         // 🔐 GitLab Token
 
         String gitlabToken = "glpat-28eFqsNLmXKJxuPbaYBZ";
